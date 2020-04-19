d56e7ca515c8072d9c7c3fab618ce940597252e9
Julfikar <julfikar6262@gmail.com>
2020-04-20 01:32:09 +0600

    token update

--- api_basic/signals.py
+++ api_basic/signals.py
@@ -1,12 +1,11 @@
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
-@receiver(post_save, sender=User)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
     if created:
-        token = Token.objects.create(user=user)
+        token = Token.objects.create(user=instance)
         print(token)
